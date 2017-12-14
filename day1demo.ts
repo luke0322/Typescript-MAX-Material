5572a3c4b785b691fefc3967ec130d5b243afa1c
Lucas Hinegardner <lucas.hinegardner@gmail.com>
2017-12-14 11:45:38 -0500

    finished first demo

--- day1demo.ts
+++ day1demo.ts
@@ -8,6 +8,9 @@ function xyz(
     return (a + b + c); //cannot make a toString call, because your return type is number
 }
 //can declare return void with :void
+function b4j():void{
+
+}
 
 
 let x: number = xyz(1, 2, 3); //we are capturing a function call and assigning it to x
@@ -24,7 +27,7 @@ b = a+1;
 
 let c = 0; 
 c = a + ""1"; 
-//typescript now knows that is a is a number, cannot assign a string to a number
+//typescript now knows that is c is a number, cannot assign a string to a number
 c = a + 1; //this works because c is a number
 //we'd rather catch that error at transpile time than at runtime
 
@@ -43,3 +46,21 @@ let g: number[] = [1,2,3]; //array of numbers declared here, cannot put string i
 let i: number[];
 let h: number[] = [1,"2",3]; //javascript can still be generated from incorrect typescript
 // in typescript we must type our arrays before populating them
+
+enum Color { Red, Green, Blue };
+let d: Color = Color.Green; //enums used in typescript are much cleaner than their javascript counterpart
+
+let variedArray: any = [1, "2", true, [], {}]; //benefits of any = gives f the ability to be any type in typescript
+//can leave off any, but it is documenting here for later use (your intention for the array)
+//varied array can be changed to a different data type
+
+let variedArray2: any[] = [1, "2", true, [], {}]; //specifies it as any ARRAY,cannot be changed to another data type
+
+console.log(5 == "5"); //gives you a typescript error
+
+const cst = 0; //cannot be changed afterwards
+cst = 1; //not allowed 
+
+//good example in application of constant is::: const url = http://localhost:8080/
+											  //url + "/Users/List"
+
